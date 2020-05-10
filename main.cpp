4a9233cedfef59965da8a9244af69869f91c019e
zhudahai <nextleave@gmail.com>
2020-05-10 21:30:52 +0800

    test git.

--- main.cpp
+++ main.cpp
@@ -12,6 +12,11 @@ int main() {
     cout << "Hello, World!" << endl;
     thread myjob(myprint);
     myjob.join();
+    for (int i{0}; i < 5; i++) {
+        cout << i<<endl;
+    }
+    int a[]{3, 5, 8};
+    cout << a;
     return 0;
 }
 
